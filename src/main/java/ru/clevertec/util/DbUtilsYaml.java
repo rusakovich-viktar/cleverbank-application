b92a9ec99f183ea7e51813bd870f6ec6427f2f3a
Viktar Rusakovich <mrisviz.rus@gmail.com>
2023-08-26 12:46:08 +0300

    add yaml settings

---
+++ src/main/java/ru/clevertec/util/DbUtilsYaml.java
@@ -0,0 +1,33 @@
+package ru.clevertec.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+import org.yaml.snakeyaml.Yaml;
+
+public class DbUtilsYaml {
+    public static Connection connection() {
+        Connection connection = null;
+        try {
+            Yaml yaml = new Yaml();
+            InputStream inputStream = new FileInputStream("C:\\Java\\ConsoleAppSimple\\simpleSQL\\src\\main\\resources\\database.yml");
+            Map<String, Map<String, String>> configMap = yaml.load(inputStream);
+            Map<String, String> dbConfig = configMap.get("database");
+            String url = dbConfig.get("url");
+            String username = dbConfig.get("username");
+            String password = dbConfig.get("password");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (IOException e) {
+            System.out.println("IOException");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQLException");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
